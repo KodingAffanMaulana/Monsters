c9602b42858a7f1f635a887ea0d6a6beb2edf1f5
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2023-07-30 20:44:16 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react"
 import './App.css';
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -7,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -17,23 +20,35 @@ class App extends Component {
         this.setState(
           () => {
             return { monsters: users };
-          },
-          () => { console.log(this.state) }
+          }
         )))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this
+
+    const filterMonsters = monsters.filter((monsters) => {
+      return monsters.name.toLowerCase().includes(searchField)
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          {this.state.monsters.map((monster, index) => {
-            return (
-              <div>
-                <h1 key={index}>{monster.name}</h1>
-              </div>
-            )
-          })}
-        </header>
+        <h1 className="app-title">Monsters Affan</h1>
+
+        <SearchBox
+          className="search-box"
+          placeholder="search monster"
+          onChangeHandler={onSearchChange}
+        />
+        <CardList monsters={filterMonsters} />
       </div>
     );
   }
